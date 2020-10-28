d862195b38b462a8956b6645583e3d94d7fa0c15
NeProgramist <zaskozhenya@gmail.com>
2020-10-28 13:15:09 +0200

    feat(core): create postfix algorithm [./implementation.go]

--- implementation.go
+++ implementation.go
@@ -1,9 +1,57 @@
 package lab2
 
-import "fmt"
+import (
+	"errors"
+	"math"
+	"strconv"
+	s "strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func CalculatePostfix(input string) (float64, error) {
+	stack := &Stack{nil, 0}
+	operators := Filter(s.Split(input, " "), func(c string) bool {
+		return c != "" && c != " "
+	})
+
+	for _, v := range operators {
+		if num, e := strconv.ParseFloat(v, 64); e == nil {
+			stack.push(&Node{num, nil})
+		} else {
+			left, _ := stack.pop()
+			right, err := stack.pop()
+			if err != nil {
+				return 0.0, err
+			}
+			switch v {
+			case "-":
+				node := &Node{right.value - left.value, nil}
+				stack.push(node)
+			case "+":
+				node := &Node{right.value + left.value, nil}
+				stack.push(node)
+			case "*":
+				node := &Node{right.value * left.value, nil}
+				stack.push(node)
+			case "/":
+				if left.value == 0 {
+					return 0.0, errors.New("denominator cannot be 0")
+				}
+				node := &Node{right.value / left.value, nil}
+				stack.push(node)
+			case "^":
+				node := &Node{math.Pow(right.value, left.value), nil}
+				stack.push(node)
+			default:
+				return 0.0, errors.New("unexpected type")
+			}
+		}
+	}
+	if res, err := stack.pop(); err == nil {
+		if stack.length != 0 {
+			return 0.0, errors.New("stack is not empty; mistake in expresion")
+		}
+		return res.value, nil
+	} else {
+		return 0.0, err
+	}
 }
